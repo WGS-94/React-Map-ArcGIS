f4e7be7e129ae1c207397443343e9ff093e548a3
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-07-12 23:07:58 -0300

    :fire: Display a map in application

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,12 @@
-import React, { useRef, useEffect }from 'react';
-import { loadModules } from 'esri-loader';
+import React, { useRef}from 'react';
+import { useCreateMap } from '../hooks/createMap';
 
 export default function Map() {
 
-  const MapEl = useRef(null);
+  const MapRef = useRef(null);
+  useCreateMap(MapRef);
   
-  useEffect(() => {
-    let view;
-
-    loadModules(["esri/views/MapView", "esri/views/WebMap"], {css: true})
-      .then((MapView, WebMap) => {
-      const webmap = new WebMap({
-        basemap: 'topo-vector'
-      })
-
-      view = new MapView({
-        map: webmap,
-        center: [],
-        zoom: 8,
-        // use of ref as container
-        container: MapEl.current
-      })
-      
-      return () => {
-        // close the map
-        if(!!view) {
-          view.destroy();
-          view = null;
-        }
-      }
-    
-    })
-
-  });
-
   return (
-    <div style={{ height: 800}} ref={MapEl}>
-      
-    </div>
+    <div style={{ height: 800}} ref={MapRef} />
   );
 }
---
+++ src/hooks/createMap.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { loadModules } from 'esri-loader';
+
+export const useCreateMap = (MapRef) => {
+
+  useEffect(() => {
+
+    let view;
+
+    const initializedMap = async (MapRef) => {
+      const modules = ["esri/Map", "esri/views/MapView"];
+      const [Map, MapView] = await loadModules(modules);
+      const map = new Map({ basemap: 'streets-relief-vector' });
+
+      view = new MapView({
+        map: map,
+        center: [-104.82313502980763, 38.828962462600366],
+        zoom: 13,
+        // use of ref as container
+        container: MapRef.current
+      })
+
+      //basemap: 'topo-vector'
+      //"esri/views/WebMap"
+    }
+    
+    initializedMap(MapRef);
+
+    return () => view?.destroy();
+
+    /*return () => {
+        // close the map
+        if(!!view) {
+          view.destroy();
+          view = null;
+        }
+      }*/
+
+  }, [MapRef]);
+
+}
